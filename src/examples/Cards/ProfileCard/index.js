c94bf6942deb6471a64ddcb44df0a936aa729d76
He Lin <helinchooi@gmail.com>
2022-01-23 04:29:46 +0800

    Modify My advisor page by adding info card

---
+++ src/examples/Cards/ProfileCard/index.js
@@ -0,0 +1,112 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// prop-types is a library for typechecking of props
+import PropTypes from "prop-types";
+
+// @mui material components
+import Card from "@mui/material/Card";
+import Divider from "@mui/material/Divider";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+
+function ProfileCard({ color, jobTitle, name, percentage, icon }) {
+  return (
+    <Card sx={{ marginBottom: { xs: 2 }, marginRight: { xs: 0, md: 2 } }}>
+      <MDBox display="flex" justifyContent="space-between" pt={1} px={2}>
+        <MDBox
+          variant="gradient"
+          bgColor={color}
+          color={color === "light" ? "dark" : "white"}
+          coloredShadow={color}
+          borderRadius="xl"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          width="4rem"
+          height="4rem"
+          mt={-3}
+        >
+          <img src={icon} alt="profile" height="100%" />
+        </MDBox>
+        <MDBox textAlign="right" lineHeight={1.25}>
+          <MDTypography variant="button" fontWeight="light" color="text">
+            {jobTitle}
+          </MDTypography>
+          <MDTypography variant="h4">{name}</MDTypography>
+        </MDBox>
+      </MDBox>
+      <Divider />
+      <MDBox pb={2} px={2}>
+        <MDTypography component="p" variant="button" color="text" display="flex">
+          <MDTypography
+            component="span"
+            variant="button"
+            fontWeight="bold"
+            color={percentage.color}
+          >
+            {percentage.amount}
+          </MDTypography>
+          {percentage.label}
+        </MDTypography>
+      </MDBox>
+    </Card>
+  );
+}
+
+// Setting default values for the props of ProfileCard
+ProfileCard.defaultProps = {
+  color: "info",
+  percentage: {
+    color: "success",
+    text: "",
+    label: "",
+  },
+};
+
+// Typechecking props for the ProfileCard
+ProfileCard.propTypes = {
+  color: PropTypes.oneOf([
+    "primary",
+    "secondary",
+    "info",
+    "success",
+    "warning",
+    "error",
+    "light",
+    "dark",
+  ]),
+  name: PropTypes.string.isRequired,
+  jobTitle: PropTypes.string.isRequired,
+  percentage: PropTypes.shape({
+    color: PropTypes.oneOf([
+      "primary",
+      "secondary",
+      "info",
+      "success",
+      "warning",
+      "error",
+      "dark",
+      "white",
+    ]),
+    amount: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    label: PropTypes.string,
+  }),
+  icon: PropTypes.node.isRequired,
+};
+
+export default ProfileCard;
